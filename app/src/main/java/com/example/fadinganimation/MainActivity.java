033007d47157c6606ff556fc4b7b1807abbbdb27
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-03-15 19:52:06 +0600

    fading animation

---
+++ app/src/main/java/com/example/fadinganimation/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.fadinganimation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
