0066c4b76e9e99f5c36ccec739d053cd3904e503
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-03-20 11:29:47 +0600

    writing codes

--- app/src/main/java/com/example/fadinganimation/MainActivity.java
+++ app/src/main/java/com/example/fadinganimation/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.fadinganimation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView logoImage;
+    ImageView ezioImage
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logoImage = (ImageView) findViewById(R.id.imageViewLogo);
+        ezioImage = (ImageView) findViewById(R.id.imageViewEzio);
+    }
+
+    public void imageClicked(View v) {
+
     }
 }
