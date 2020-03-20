3fda817b4dfcb86ee3b3f95f9c9b00d2dac002c7
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-03-20 11:44:55 +0600

    writing codes

--- app/src/main/java/com/example/fadinganimation/MainActivity.java
+++ app/src/main/java/com/example/fadinganimation/MainActivity.java
@@ -24,6 +24,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void imageClicked(View v) {
-        
+        if(1 == selectedImage) {
+            selectedImage = 2;
+            logoImage.animate().alpha(0).setDuration(2000);
+            ezioImage.animate().alpha(1).setDuration(2000);
+        } else {
+            selectedImage = 1;
+            ezioImage.animate().alpha(0).setDuration(2000);
+            logoImage.animate().alpha(1).setDuration(2000);
+        }
     }
 }
